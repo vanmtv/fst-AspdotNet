dc15730eee6ac4ab5e10c82d88358e743c11548b
Vanessa Regina <44881631+vanmtv@users.noreply.github.com>
2019-10-08 21:23:37 -0300

    adicionar pessoas a lista de controller

--- PrimeiroProjetoWeb/Controllers/PessoaController.cs
+++ PrimeiroProjetoWeb/Controllers/PessoaController.cs
@@ -9,6 +9,20 @@ namespace PrimeiroProjetoWeb.Controllers
         public IActionResult Index()
         {
             var pessoas = new List<PessoaViewModel>();
+            pessoas.Add(
+                new PessoaViewModel()
+                {
+                    Nome = "Ana Carolina",
+                    Idade = 28,
+                    Telefone = "11 2525-2525"
+                });
+            pessoas.Add(
+                new PessoaViewModel()
+                {
+                    Nome = "Joao da Silva",
+                    Idade = 30,
+                    Telefone = "11 2525-2525"
+                });
             return View();
         }
     }
